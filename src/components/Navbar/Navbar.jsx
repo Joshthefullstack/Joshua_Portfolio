07d434cd23352d5ab428bdab471054d1bec03625
Joshthefullstack <104472181+Joshthefullstack@users.noreply.github.com>
2025-02-12 02:40:37 +0000

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           initial={{ opacity: 0, scale: 0.5}}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-         >Dave</motion.span>
+         >Josh</motion.span>
         <div className="social-links">
           <a href="https://github.com/Joshthefullstack/"><img src="./assets/github-svgrepo-com(1).png" alt=""/></a>
           <a href="www.linkedin.com/in/josh-theedev-28426629a"><img src="./assets/linkedin-svgrepo-com.png" alt=""/></a>
